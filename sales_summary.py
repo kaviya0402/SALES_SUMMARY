fbd871113c7e650c9ba683f20fbea972b4617306
kaviya0402 <kaviyamib@gmail.com>
2026-06-08 07:00:11 -0400

    sales_summary uploaded

---
+++ sales_summary.py
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect("sales_data.db")
+
+query = """
+SELECT product,
+       SUM(quantity) AS total_qty,
+       SUM(quantity * price) AS revenue
+FROM sales
+GROUP BY product
+"""
+
+df = pd.read_sql_query(query, conn)
+
+print(df)
+
+df.plot(kind='bar', x='product', y='revenue')
+plt.title("Revenue by Product")
+plt.show()
+
+conn.close()
